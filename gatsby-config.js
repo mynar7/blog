3736d41a51bc8efa5d150d0a7fff5e935161c26d
Lee Warrick <Lee.Warrick@realtruck.com>
2019-08-15 10:00:16 -0400

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,12 +74,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-144549907-2`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
