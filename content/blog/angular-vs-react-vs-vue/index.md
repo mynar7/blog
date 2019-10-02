cbdb05ac4b80fe2b1e4073c80adce9aa3f266322
Lee Warrick <Lee.Warrick@realtruck.com>
2019-10-02 16:39:42 -0400

    start post

---
+++ content/blog/angular-vs-react-vs-vue/index.md
@@ -0,0 +1,5 @@
+---
+title: "Front-end Fisticuffs: Angular vs React vs Vue in 2019"
+date: 2019-10-04T10:00:00-0400
+---
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ class BlogIndex extends React.Component {
           const title = node.frontmatter.title || node.fields.slug
           return (
             <div key={node.fields.slug}>
-              <h3
+              <h2
                 style={{
                   marginBottom: rhythm(1 / 4),
                   color: 'inherit'
@@ -32,7 +32,7 @@ class BlogIndex extends React.Component {
                 <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
                   {title}
                 </Link>
-              </h3>
+              </h2>
               <small>{node.frontmatter.date}</small>
               <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
             </div>
